b22d9a25b92a8bfb3574bc3e330ecd38fb0b9dc3
Bernard REMOND <nbremond@laposte.net>
2016-03-28 22:53:05 +0200

    Added files via upload

--- Artnet_light_control.py
+++ Artnet_light_control.py
@@ -3,9 +3,8 @@
 #
 #  More details.
 
-    
- """
- To activate a specific settings for the application, run:
+"""
+To activate a specific settings for the application, run:
     source env/bin/activate
     export APP_SETTINGS="config.DevelopmentConfig"
     OR export APP_SETTINGS="config.ProductionConfig"
@@ -14,43 +13,75 @@
 """
 
 from flask import Flask, url_for
-from flask import render_template
+from flask import render_template, request,  flash,  redirect
+import os
 
+
+
+sceneList=[]
+aScene = dict(name='Douche_simple',  image='static/douche1.jpg',  page=1)
+sceneList.append(aScene)
+aScene = dict(name='Douche_froide',  image='static/douche2.jpg',  page=1)
+sceneList.append(aScene)
+aScene = dict(name='Douche_chaude',  image='static/douche3.jpg',  page=1)
+sceneList.append(aScene)
+
+aScene = dict(name='Bain_simple',  image='static/douche1.jpg',  page=2)
+sceneList.append(aScene)
+aScene = dict(name='Bain_froid',  image='static/douche2.jpg',  page=2)
+sceneList.append(aScene)
+aScene = dict(name='Bain_chaude',  image='static/douche3.jpg',  page=2)
+sceneList.append(aScene)
+
+MAX_PAGES = 4
+imageList = ['static/douche1.jpg',  'static/douche2.jpg',  'static/douche3.jpg',  'static/douche4.jpg']
+currentPage = 1
+
+
+# Create the application
 app = Flask(__name__)
+
+# Run in development mode... for now
+os.system('export APP_SETTINGS="config.DevelopmentConfig"')
+
+# Load app configuration
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 
-@app.route('/')
-def mainLayout():
-    """short description of the function square
-
-    longish explanation: returns the square of a: :math:`a^2`
-
-    :param a: an input argument
-
-    :returns: a*a
-    """
-    with app.test_request_context():
-        print url_for('setup')
-        print url_for('about')
-        print url_for('page', page='Page1')    
-
-    return "ArtNet Light Control - main layout"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    #print(sceneList)
+    return render_template('layout_1.html',  buttonList=sceneList,  imageList=imageList,  page=currentPage)
 
 
-@app.route('/page/<page>')
-def pageLayout(page=None):
-    """short description of the function square
+@app.route('/scene', methods = ['POST'])
+def scene():
+    print("POST - Scene1")
+    sceneName = request.form['Scene']
+    print(sceneName)
+    for i in sceneList:
+        #print(i)
+        #print(i['name'])
+        if (i['name'] == sceneName):
+            currentScene = i
+            print('-->FOUND: %s' % currentScene)
+            break
+    #print(currentScene)
+    return redirect('/')
 
-    longish explanation: returns the square of a: :math:`a^2`
+@app.route('/next', methods = ['GET'])
+def next():
+    global currentPage
+    currentPage = min(currentPage+1,  MAX_PAGES)
+    print("GET - next - %s" %  currentPage)
+    return redirect('/')
 
-    :param a: an input argument
-
-    :returns: a*a
-    """
-#    return "ArtNet Light Control - layout %s" % page
-    return render_template('layout_1.html', page=page)
-   
+@app.route('/previous', methods = ['GET'])
+def previous():
+    global currentPage
+    currentPage = max(currentPage-1,  1)
+    print("GET - previous - %s" %  currentPage)
+    return redirect('/')    
 
 @app.route('/setup')
 def setup():
@@ -73,4 +104,4 @@ if __name__ == '__main__':
 
     :returns: none
     """
-    app.run('192.168.0.0')
+    app.run('0.0.0.0')
