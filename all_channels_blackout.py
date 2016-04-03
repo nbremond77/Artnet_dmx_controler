fb354255869107631025a3a54459782c72408877
Bernard REMOND <nbremond@laposte.net>
2016-04-03 22:56:12 +0200

    Added files via upload

---
+++ all_channels_blackout.py
@@ -0,0 +1,14 @@
+# From gitHub : philchristensen/python-artnet
+# https://github.com/philchristensen/python-artnet
+import logging
+
+from artnet import dmx
+
+log = logging.getLogger(__name__)
+
+def main(config, controller=None):
+	log.info("Running script %s" % __name__)
+	q = controller or dmx.Controller(config.get('base', 'address'), nodaemon=False)
+	q.add(iter([[0] * 512]))
+	if not controller:
+		q.start()
